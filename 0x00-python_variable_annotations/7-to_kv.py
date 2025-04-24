f2ea8dce2afb5b276a3f2b79d56f06d0e86b671a
jmodhiambo <jmodhiambo9346@gmail.com>
2025-04-25 01:28:53 +0300

    Task 7

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Takes a string and integer or float and return a tuple."""
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """Takes in a string and int or float and returns a tuple."""
     return (k, v ** 2)

a2652bd6aef740dc2528d36552909140241bced2
jmodhiambo <jmodhiambo9346@gmail.com>
2025-04-24 23:53:58 +0300

    Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Takes a float as an argument and returns a string."""
+
+
+def to_str(n: float) -> str:
+    """Returns a string reprsentation of float."""
+    return str(n)

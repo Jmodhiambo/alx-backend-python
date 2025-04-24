db8b0dce0de57037302c737eccaf6ab1b8555dde
jmodhiambo <jmodhiambo9346@gmail.com>
2025-04-25 00:41:22 +0300

    Task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Takes is a list of floats and returns their sum in float"""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Sums float values in a input_list."""
+    return sum(input_list)
